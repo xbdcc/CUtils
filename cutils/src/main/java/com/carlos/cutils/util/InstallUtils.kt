97216befaffc993c5c57e6c4a0cfb59b59d5a3ee
carlos <363465939@qq.com>
2019-02-02 15:19:00 +0800

    Add net and install utils.

---
+++ cutils/src/main/java/com/carlos/cutils/util/InstallUtils.kt
@@ -0,0 +1,34 @@
+package com.carlos.cutils.util
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.support.v4.content.FileProvider
+import java.io.File
+
+/**
+ * 安装APK工具类
+ * Created by Carlos on 2018/11/14.
+ */
+internal object InstallUtils {
+
+    fun install(context: Context, apkPath: String) {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        val downFile = File(apkPath)
+        //版本在7.0以上特殊处理
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
+            //添加这一句表示对目标应用临时授权该Uri所代表的文件
+            intent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+            //参数1 上下文, 参数2 Provider主机地址 和配置文件中保持一致   参数3  共享的文件
+            val uri = FileProvider.getUriForFile(context, context.packageName+ ".fileProvider", downFile)
+            intent.setDataAndType(uri, "application/vnd.android.package-archive")
+        }else {
+//            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.setDataAndType(Uri.fromFile(downFile), "application/vnd.android.package-archive")
+        }
+        context.startActivity(intent)
+    }
+
+}
---
+++ cutils/src/main/java/com/carlos/cutils/util/NetUtils.kt
@@ -0,0 +1,115 @@
+package com.carlos.cutils.util
+
+import android.os.Handler
+import java.io.*
+import java.net.HttpURLConnection
+import java.net.URL
+
+/**
+ * Created by Carlos on 2018/11/30.
+ */
+internal class NetUtils {
+
+    fun get(url: String) {
+        try {
+            val url = URL(url)
+            val httpURLConnection = url.openConnection()
+            httpURLConnection.connectTimeout = 10*1000
+            httpURLConnection.connect()
+            // 通过InputStreamReader读取流，设置编码防止中文乱码
+            val inputStreamReader = InputStreamReader(httpURLConnection.getInputStream(), "GBK")
+            val bufferedReader = BufferedReader(inputStreamReader)
+            var resultData = ""
+            var inputLine = bufferedReader.readLine()
+            while(inputLine!=null) {
+                resultData += inputLine
+                inputLine = bufferedReader.readLine()
+            }
+//            Log.i("Http通信之HttpURLConnection", "get方法\n" + resultData);
+        }catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    fun post(url: String, data: String): String {
+        try {
+            val url = URL(url)
+            val httpURLConnection = url.openConnection() as HttpURLConnection
+            httpURLConnection.doInput = true
+            httpURLConnection.doOutput = true
+            httpURLConnection.requestMethod = "POST"
+            httpURLConnection.connect()
+
+            val dataOutputStream = DataOutputStream(httpURLConnection.outputStream)
+            dataOutputStream.write(data.toByteArray())
+            dataOutputStream.flush()
+            dataOutputStream.close()
+
+            val inputStreamReader = InputStreamReader(httpURLConnection.inputStream, "UTF-8")
+            val bufferedReader = BufferedReader(inputStreamReader)
+            val stringBuffer = StringBuffer()
+            var temp = bufferedReader.readLine()
+            while(temp!=null) {
+                stringBuffer.append(temp)
+                temp = bufferedReader.readLine()
+            }
+            bufferedReader.close()
+            inputStreamReader.close()
+            httpURLConnection.disconnect()
+            LogUtils.d("test:" + stringBuffer.toString())
+            return stringBuffer.toString()
+        }catch (e: Exception) {
+            LogUtils.d("error",e)
+            e.printStackTrace()
+            return ""
+        }
+    }
+
+    fun download(url: String, filePath: String,handler: Handler) {
+        try {
+            val file = File(filePath)
+            LogUtils.d("filePath:" + file.path)
+
+            val url = URL(url)
+            val httpURLConnection = url.openConnection() as HttpURLConnection
+
+            var hasDownSize = 0L
+
+            if (file.exists()) {
+                hasDownSize = file.length()
+
+                // 设置断点续传的开始位置
+                httpURLConnection.setRequestProperty("Range", "bytes=" + file.length() + "-")
+            }
+            val code = httpURLConnection.responseCode
+            LogUtils.d("responseCode:$code")
+            if (code == 200 || code == 206) {
+
+                val inputStream = httpURLConnection.inputStream
+                val fileOutputStream = FileOutputStream(file, true)
+
+                val totoalSize = httpURLConnection.contentLength + hasDownSize
+                val buffer = ByteArray(102400)
+                var readByte = inputStream.read(buffer)
+
+                while (readByte!=-1) {
+                    fileOutputStream.write(buffer, 0, readByte)
+                    hasDownSize += readByte
+                    readByte = inputStream.read(buffer)
+
+                    val percent = hasDownSize * 100.0f/ totoalSize
+                    handler.sendEmptyMessage(percent.toInt())
+                    LogUtils.d("percent:$percent----down:$hasDownSize---total:$totoalSize")
+                }
+            }
+            else if (code == 416) {
+                handler.sendEmptyMessage(100)
+            }else
+                handler.sendEmptyMessage(0)
+        }catch (e: Exception) {
+            handler.sendEmptyMessage(-1)
+        }
+
+    }
+
+}
